177779fa92ac959c114cb665c84292b39e3b7f28
Zachary <zacharypreston1996@protonmail.com>
2021-10-06 00:09:48 -0400

    Added comments for documentation purposes

--- Client.py
+++ Client.py
@@ -1,16 +1,31 @@
 import socket
 
 def clientProgram():
+
+    # Get the hostname and establish the port number
+    # the port number should be the same as the server
     host = socket.gethostname()
     port = 8000
+
+    # Create an instance of the socket object class
+    # Connect to server using same port number
+    # Input prompt -> saved to message as str
     clientSocket = socket.socket()
     clientSocket.connect((host, port))
     message = input(' -> ')
+
+    # While input is not 'bye', continue to communicate
+    # with the server. 'bye' terminates the communication
+    # send the message, and then receive the message,
+    # print it to the screen, and finally close the
+    # connection when 'bye' is entered
     while (message.lower().strip() != 'bye'):
         clientSocket.send(message.encode())
         data = clientSocket.recv(1024).decode()
         print( 'Received from server: ' + data )
         message = input(' -> ')
+
+    # Close the connection with the server
     clientSocket.close()
 
 if __name__ == '__main__':
--- Server.py
+++ Server.py
@@ -1,15 +1,28 @@
 import socket
 
 def serverProgram():
+
+    # Get the hostname and establish port number
     host = socket.gethostname()
     port = 8000
 
+    # Create an instance of the socket object class
+    # Use bind() function to assign an IP address and port number to socket instance
     serverSocket = socket.socket()
     serverSocket.bind((host, port))
 
+    # Use listen() function to listen for incoming connections
+    # Accept function returns two values (host, port) for IPv4
+    # and 'accepts' the connection
     serverSocket.listen(2)
     connection, address = serverSocket.accept()
     print('Connection from ' + str(address))
+
+    # Now a connection is established between client/server
+    # While the client does not terminate the connection,
+    # The server will receive data from client, and if the data
+    # is not NULL, then the server will send data back to client
+    # this will continue until the client terminates the connection
     while True:
         data = connection.recv(1024).decode()
         if not data:
@@ -17,6 +30,8 @@ def serverProgram():
         print('From connected user: ' + str(data))
         data = input(' -> ')
         connection.send(data.encode())
+
+    # Finally, the connection will be closed
     connection.close()
 
 if __name__ == '__main__':
