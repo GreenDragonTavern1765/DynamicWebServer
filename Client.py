3d09f61895eba2c7f64b7305b08ec59106d20a46
Zachary <zacharypreston1996@protonmail.com>
2021-10-06 22:19:36 -0400

    Modified the server and client python files. Server is not prompting to return a message, but instead is rather listening and then returning the html file when the user enters in the file name

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 import socket
+from datetime import datetime
 
 def clientProgram():
 
@@ -12,21 +13,51 @@ def clientProgram():
     # Input prompt -> saved to message as str
     clientSocket = socket.socket()
     clientSocket.connect((host, port))
+    print('Connection Established...')
+
     message = input(' -> ')
+    clientSocket.send(message.encode())
+    data = clientSocket.recv(1024).decode()
 
     # While input is not 'bye', continue to communicate
     # with the server. 'bye' terminates the communication
     # send the message, and then receive the message,
     # print it to the screen, and finally close the
     # connection when 'bye' is entered
-    while (message.lower().strip() != 'bye'):
+    while (data == '--> ERROR: File not found'):
+        print('--> ERROR: File not found')
+        message = input(' -> ')
         clientSocket.send(message.encode())
         data = clientSocket.recv(1024).decode()
-        print( 'Received from server: ' + data )
-        message = input(' -> ')
+    print(data)
 
     # Close the connection with the server
+    print('Connection Terminated... ')
     clientSocket.close()
 
+def readFromFile(fileName):
+    try:
+        file = open(fileName, "r")
+    except FileNotFoundError:
+        return None
+    content = file.read()
+    file.close()
+    return content
+
+def makeHTML(contents):
+    list = contents.splitlines()
+    for i in range(0, len(list)):
+        if list[i] == '</body>':
+            break
+    now = datetime.now()
+    ipAddress = "\t<p>My IP Address is " + str(socket.gethostbyname(socket.gethostname())) + "</p>"
+    date = "\t<p>Date and Time is " + str(now) + "</p>"
+    list.insert(i, date)
+    list.insert(i, ipAddress)
+    for i in range(0, len(list)):
+        print(list[i])
+
 if __name__ == '__main__':
-    clientProgram()
+    #clientProgram()
+    content = readFromFile('IPAddress.html')
+    makeHTML(content)
--- Server.py
+++ Server.py
@@ -16,7 +16,7 @@ def serverProgram():
     # and 'accepts' the connection
     serverSocket.listen(2)
     connection, address = serverSocket.accept()
-    print('Connection from ' + str(address))
+    print('Connection Established: ' + str(address))
 
     # Now a connection is established between client/server
     # While the client does not terminate the connection,
@@ -24,15 +24,27 @@ def serverProgram():
     # is not NULL, then the server will send data back to client
     # this will continue until the client terminates the connection
     while True:
-        data = connection.recv(1024).decode()
-        if not data:
+        clientData = connection.recv(1024).decode()
+        if not clientData:
             break
-        print('From connected user: ' + str(data))
-        data = input(' -> ')
-        connection.send(data.encode())
+        serverData = readFromFile(clientData)
+        if serverData is None:
+            connection.send('--> ERROR: File not found'.encode())
+        else:
+            connection.send(serverData.encode())
 
     # Finally, the connection will be closed
+    print('Connection Terminated: ' + str(address))
     connection.close()
+
+def readFromFile(fileName):
+    try:
+        file = open(fileName, "r")
+    except FileNotFoundError:
+        return None
+    content = file.read()
+    file.close()
+    return content
 
 if __name__ == '__main__':
     serverProgram()
