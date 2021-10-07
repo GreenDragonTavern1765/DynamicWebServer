7ff4b2bce894fba75c874fa3211853b1052e60ea
Zachary <zacharypreston1996@protonmail.com>
2021-10-06 23:06:54 -0400

    Comments added to give clarity to the client and server files

--- Client.py
+++ Client.py
@@ -1,63 +1,74 @@
+# Import socket library as well as the datetime
 import socket
 from datetime import datetime
 
+# Client program, working the client side of the client/server architecture, the client will
+# make a request to the server for the html file contents, and once the connection is established
+# and the file name is added, the server will return the HTML file contents for it to be changed
 def clientProgram():
 
-    # Get the hostname and establish the port number
-    # the port number should be the same as the server
+    # Get the host name and establish the port number the client/server will communicate on
     host = socket.gethostname()
     port = 8000
 
-    # Create an instance of the socket object class
-    # Connect to server using same port number
-    # Input prompt -> saved to message as str
+    # Instance of the socket object, and the connect function call, using host and port number
+    # finally, once connection is established, print that the connection is established
     clientSocket = socket.socket()
     clientSocket.connect((host, port))
     print('Connection Established...')
 
+    # The client will prompt to enter a value, this will continue to ask until the name of the
+    # HTML file contents are to be downloaded, finally data received is saved as a string
     message = input(' -> ')
     clientSocket.send(message.encode())
     data = clientSocket.recv(1024).decode()
 
-    # While input is not 'bye', continue to communicate
-    # with the server. 'bye' terminates the communication
-    # send the message, and then receive the message,
-    # print it to the screen, and finally close the
-    # connection when 'bye' is entered
+    # While the user does not enter in the HTML file name, the file does not exist, so the
+    # error message will be returned each time, until the user enters in 'IPAddress.html'
+    # Call send function with the message input by the client through the client socket
+    # finally, once the HTML file contents are transferred successfully, call makeHTML()
     while (data == '--> ERROR: File not found'):
         print('--> ERROR: File not found')
         message = input(' -> ')
         clientSocket.send(message.encode())
         data = clientSocket.recv(1024).decode()
-    print(data)
+    makeHTML(data)
 
-    # Close the connection with the server
+    # Once the HTML file contents are added, close the connection to the server file
+    print('File successfully transfered...')
     print('Connection Terminated... ')
     clientSocket.close()
 
-def readFromFile(fileName):
-    try:
-        file = open(fileName, "r")
-    except FileNotFoundError:
-        return None
-    content = file.read()
-    file.close()
-    return content
-
+# The file contents transferred over the clientsocket are then input into makeHTML()
 def makeHTML(contents):
+
+    # Make a list of the split lines from the content of the file content transfer
+    # and the iterate the list until the </body> is found, then break
     list = contents.splitlines()
     for i in range(0, len(list)):
         if list[i] == '</body>':
             break
+
+    # Now is the date and time of right now according to the computer internal clock
+    # then the messages are added to the HTML file contents, according to list index above
     now = datetime.now()
     ipAddress = "\t<p>My IP Address is " + str(socket.gethostbyname(socket.gethostname())) + "</p>"
     date = "\t<p>Date and Time is " + str(now) + "</p>"
+
+    # Insert both into the list, and the join the list together with the join function,
+    # finally write the changes to the final.html file
     list.insert(i, date)
     list.insert(i, ipAddress)
-    for i in range(0, len(list)):
-        print(list[i])
+    result = '\n'.join(list)
+    writeHTML(result)
 
+# Writes the contents from the makeHTML() function to another html file
+# that is updated to include date/time and IP address of host
+def writeHTML(contents):
+    file = open("final.html", "w")
+    file.write(contents)
+    file.close()
+
+# Main function
 if __name__ == '__main__':
-    #clientProgram()
-    content = readFromFile('IPAddress.html')
-    makeHTML(content)
+    clientProgram()
--- Server.py
+++ Server.py
@@ -1,28 +1,34 @@
+# Import the socket library to allow for network communications
 import socket
 
+# Server function, that will serve as the server that listens for
+# clients to connect and to request file contents, that the server will
+# then send over the sockets that are established
 def serverProgram():
 
-    # Get the hostname and establish port number
+    # Get the host name of the device as well as the port number
+    # they are going to communicate over
     host = socket.gethostname()
     port = 8000
 
-    # Create an instance of the socket object class
-    # Use bind() function to assign an IP address and port number to socket instance
+    # Create an instance of the socket class, and use the bind()
+    # function to bind(), since this this a server, bind() must be used
+    # to establish the port number, while client doesn't need that
     serverSocket = socket.socket()
     serverSocket.bind((host, port))
 
-    # Use listen() function to listen for incoming connections
-    # Accept function returns two values (host, port) for IPv4
-    # and 'accepts' the connection
+    # Set the server to listen for hosts to connect and make requests
+    # then once a host connects, server will accept, and connection established
     serverSocket.listen(2)
     connection, address = serverSocket.accept()
     print('Connection Established: ' + str(address))
 
-    # Now a connection is established between client/server
-    # While the client does not terminate the connection,
-    # The server will receive data from client, and if the data
-    # is not NULL, then the server will send data back to client
-    # this will continue until the client terminates the connection
+    # While there is a connection established with client, this loop will continue
+    # Client will send data to server, which connection will receive and decode
+    # as long as the client data is not None, continue on, serverData will be the
+    # file name that the client is trying to get contents of to load on browser
+    # If the readFile() returns none, the file doesn't exist, send error message
+    # else send to client the file contents
     while True:
         clientData = connection.recv(1024).decode()
         if not clientData:
@@ -33,10 +39,14 @@ def serverProgram():
         else:
             connection.send(serverData.encode())
 
-    # Finally, the connection will be closed
+    # Finally close the connection
+    print('File contents successfully transfered...')
     print('Connection Terminated: ' + str(address))
     connection.close()
 
+# Read from the file, and if file cannot be found, return None,
+# which will prompt client to enter a different file name, otherwise
+# will return the file contents, that will be sent back to the client
 def readFromFile(fileName):
     try:
         file = open(fileName, "r")
@@ -46,5 +56,6 @@ def readFromFile(fileName):
     file.close()
     return content
 
+# Main function
 if __name__ == '__main__':
     serverProgram()
